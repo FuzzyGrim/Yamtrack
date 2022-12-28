1e025e06f40b07da8880c0713bf3f6026b6e9522
FuzzyGrim <stromed_chariotman@aleeas.com>
2022-12-28 18:08:51 +0100

    import media async

--- app/utils/api.py
+++ app/utils/api.py
@@ -3,7 +3,7 @@ import requests
 import csv
 from aiohttp import ClientSession
 from asyncio import ensure_future, gather, run
-from django.core.files import File
+
 
 from app.models import Media
 from app.utils import helpers
@@ -18,7 +18,7 @@ def search(content, query):
         url = f"https://api.themoviedb.org/3/search/multi?api_key={TMDB_API}&query={query}&include_adult=true"
 
         response = requests.get(url).json()
-        run(tmdb_get_seasons_list(response["results"]))
+        response = run(tmdb_get_seasons_list(response["results"]))
 
     else:
         
@@ -45,7 +45,9 @@ async def tmdb_get_seasons_list(response):
             if result["media_type"] == "tv":
                 url = f"https://api.themoviedb.org/3/tv/{result['id']}?api_key={TMDB_API}"
                 task.append(ensure_future(tmdb_get_seasons(session, url, result)))
-        return await gather(*task)
+
+        await gather(*task)
+        return response
 
 
 async def tmdb_get_seasons(session, url, result):
@@ -94,73 +96,57 @@ def import_myanimelist(username, user):
 
     manga_url = f"https://api.myanimelist.net/v2/users/{username}/mangalist?fields=list_status"
     mangas = requests.get(manga_url, headers=header).json()
+    series = {"anime": animes, "manga": mangas}
 
-    bulk_add_media = []
-
-    for anime in animes["data"]:
-        if not Media.objects.filter(media_id=anime["node"]["id"], api_origin="mal", user=user).exists():
-            if anime["list_status"]["status"] == "plan_to_watch":
-                anime["list_status"]["status"] = "Planning"
-            elif anime["list_status"]["status"] == "on_hold":
-                anime["list_status"]["status"] = "Paused"
-            else:
-                anime["list_status"]["status"] = anime["list_status"]["status"].capitalize()
-
-            media = Media(
-                    media_id=anime["node"]["id"],
-                    title=anime["node"]["title"],
-                    media_type="anime",
-                    score=anime["list_status"]["score"],
-                    status=anime["list_status"]["status"],
-                    api_origin="mal",
-                    user=user,
-                )
-
-            img_temp = helpers.get_image_temp(anime["node"]["main_picture"]["medium"])
-            if anime["node"]["main_picture"]["medium"] == "":
-                media.image.save(f"none.svg", File(img_temp), save=False)
-            else:
-                media.image.save(f'anime-{anime["node"]["main_picture"]["medium"].rsplit("/", 1)[-1]}', File(img_temp), save=False)
-
-            bulk_add_media.append(
-                media
-            )
-
-    for manga in mangas["data"]:
-        if not Media.objects.filter(edia_id=anime["node"]["id"], api_origin="mal", user=user).exists():
-            if anime["list_status"]["status"] == "plan_to_watch":
-                anime["list_status"]["status"] = "Planning"
-            elif anime["list_status"]["status"] == "on_hold":
-                anime["list_status"]["status"] = "Paused"
-            elif manga["list_status"]["status"] == "reading":
-                manga["list_status"]["status"] = "Watching"
-            else:
-                manga["list_status"]["status"] = manga["list_status"]["status"].capitalize()
-
-            media = Media(
-                    media_id=manga["node"]["id"],
-                    title=manga["node"]["title"],
-                    media_type="manga",
-                    score=manga["list_status"]["score"],
-                    status=manga["list_status"]["status"],
-                    api_origin="mal",
-                    user=user,
-                )
-            
-            img_temp = helpers.get_image_temp(anime["node"]["main_picture"]["medium"])
-
-            if manga["node"]["main_picture"]["medium"] == "":
-                media.image.save(f"none.svg", File(img_temp), save=False)
-            else:
-                media.image.save(f'manga-{manga["node"]["main_picture"]["medium"].rsplit("/", 1)[-1]}', File(img_temp), save=False)
-
-            bulk_add_media.append(
-                media
-            )
+    bulk_add_media = run(myanilist_get_media_list(series, user))
 
     Media.objects.bulk_create(bulk_add_media)
 
     return True
+
+async def myanilist_get_media_list(series, user):
+    async with ClientSession() as session:
+        task = []
+        for media_type, media_list in series.items():
+            for content in media_list["data"]:
+                if not await Media.objects.filter(media_id=content["node"]["id"], api_origin="mal", user=user).aexists():
+
+                    task.append(ensure_future(myanimelist_get_media(session, content, media_type, user)))
+
+        return await gather(*task)
+
+
+async def myanimelist_get_media(session, content, media_type, user):
+    if content["list_status"]["status"] == "plan_to_watch":
+        content["list_status"]["status"] = "Planning"
+    elif content["list_status"]["status"] == "on_hold":
+        content["list_status"]["status"] = "Paused"
+    elif content["list_status"]["status"] == "reading":
+        content["list_status"]["status"] = "Watching"
+    else:
+        content["list_status"]["status"] = content["list_status"]["status"].capitalize()
+
+    media = Media(
+            media_id=content["node"]["id"],
+            title=content["node"]["title"],
+            media_type=media_type,
+            score=content["list_status"]["score"],
+            status=content["list_status"]["status"],
+            api_origin="mal",
+            user=user,
+        )
+
+    if "main_picture" in content["node"]:
+        await helpers.download_image(session, content["node"]["main_picture"]["medium"], media_type)
+        media.image = f"images/{media_type}-{content['node']['main_picture']['medium'].rsplit('/', 1)[-1]}"
+        media.image = f"images/{media_type}-{content['node']['main_picture']['medium'].rsplit('/', 1)[-1]}"
+
+    else:
+        await helpers.download_image(session, "", media_type)
+        media.image = "images/none.svg"
+
+    return media
+
 
 
 def import_tmdb(file, user):
@@ -176,85 +162,76 @@ def import_tmdb(file, user):
     decoded_file = file.read().decode("utf-8").splitlines()
     reader = csv.DictReader(decoded_file)
 
-    reader = run(tmdb_get_extra_list(reader))
-
-    bulk_add_media = []
-
-    for row in reader:
-        if not Media.objects.filter(
-            media_id=row["TMDb ID"],
-            media_type=row["Type"],
-            api_origin="tmdb",
-            user=user,
-        ).exists():
-            seasons_score = {}
-            if row["Your Rating"] == "":
-                score = None
-            else:
-                score = float(row["Your Rating"])
-
-            if "num_seasons" in row:
-                for season in range(1, row["num_seasons"] + 1):
-                    seasons_score[season] = score
-
-            media = Media(
-                    media_id=row["TMDb ID"],
-                    title=row["Name"],
-                    media_type=row["Type"],
-                    seasons_score=seasons_score,
-                    score=score,
-                    status=status,
-                    api_origin="tmdb",
-                    user=user,
-                    num_seasons=row.get("num_seasons"),
-                    image=row.get("image"),
-                )
-
-            img_temp = helpers.get_image_temp(f"https://image.tmdb.org/t/p/w92{row['image']}")
-            if row["image"] == None:
-                media.image.save(f"none.svg", File(img_temp), save=False)
-            else:
-                media.image.save(f"tmdb-{row['image'].rsplit('/', 1)[-1]}", File(img_temp), save=False)
-
-            bulk_add_media.append(
-                media
-            )
+    bulk_add_media = run(tmdb_get_media_list(reader, user, status))
 
     Media.objects.bulk_create(bulk_add_media)
 
     return True
 
 
-async def tmdb_get_extra_list(reader):
+async def tmdb_get_media_list(reader, user, status):
     async with ClientSession() as session:
         task = []
         for row in reader:
-            if row["Type"] == "tv":
-                url = f"https://api.themoviedb.org/3/tv/{row['TMDb ID']}?api_key={TMDB_API}"
-                task.append(ensure_future(tmdb_get_extra(session, url, row)))
+            if not await Media.objects.filter(
+                media_id=row["TMDb ID"],
+                media_type=row["Type"],
+                api_origin="tmdb",
+                user=user,
+            ).aexists():
+                if row["Type"] == "tv":
+                    url = f"https://api.themoviedb.org/3/tv/{row['TMDb ID']}?api_key={TMDB_API}"
+                    task.append(ensure_future(tmdb_get_media(session, url, row, user, status)))
 
-            else:
-                url = f"https://api.themoviedb.org/3/movie/{row['TMDb ID']}?api_key={TMDB_API}"
-                task.append(ensure_future(tmdb_get_extra(session, url, row)))
+                else:
+                    url = f"https://api.themoviedb.org/3/movie/{row['TMDb ID']}?api_key={TMDB_API}"
+                    task.append(ensure_future(tmdb_get_media(session, url, row, user, status)))
 
         return await gather(*task)
 
 
-async def tmdb_get_extra(session, url, result):
+async def tmdb_get_media(session, url, row, user, status):
+
     async with session.get(url) as resp:
         response = await resp.json()
-        if "poster_path" in response:
-            result["image"] = response["poster_path"]
+            
+        seasons_score = {}
+        if row["Your Rating"] == "":
+            score = None
+        else:
+            score = float(row["Your Rating"])
 
         if "last_episode_to_air" in response:
-            result["num_seasons"] = response["last_episode_to_air"]["season_number"]
-        return result
+            for season in range(1, response["last_episode_to_air"]["season_number"] + 1):
+                seasons_score[season] = score
+
+        media = Media(
+                media_id=row["TMDb ID"],
+                title=row["Name"],
+                media_type=row["Type"],
+                seasons_score=seasons_score,
+                score=score,
+                status=status,
+                api_origin="tmdb",
+                user=user,
+                num_seasons=row.get("num_seasons"),
+                image=row.get("image"),
+            )
+
+        await helpers.download_image(session, f"https://image.tmdb.org/t/p/w92{response['poster_path']}", "tmdb")
+        
+        if response["poster_path"] == None:
+            media.image = "images/none.svg"
+        else:
+            media.image = f"images/tmdb-{response['poster_path'].rsplit('/', 1)[-1]}"
+
+        return media
 
 
 def import_anilist(username, user):
-    anime_query = """
+    query = """
     query ($userName: String){
-        MediaListCollection(userName: $userName, type: ANIME) {
+        anime: MediaListCollection(userName: $userName, type: ANIME) {
             lists {
                 isCustomList
                 entries {
@@ -272,11 +249,7 @@ def import_anilist(username, user):
                 }
             }
         }
-    }
-    """
-    manga_query = """
-    query ($userName: String) {
-        MediaListCollection(userName: $userName, type: MANGA) {
+        manga: MediaListCollection(userName: $userName, type: MANGA) {
             lists {
                 isCustomList
                 entries {
@@ -301,86 +274,62 @@ def import_anilist(username, user):
 
     url = "https://graphql.anilist.co"
 
-    animes = requests.post(url, json={"query": anime_query, "variables": variables}).json()
-    mangas = requests.post(url, json={"query": manga_query, "variables": variables}).json()
+    query = requests.post(url, json={"query": query, "variables": variables}).json()
 
     errors = []
 
-    if "errors" in animes:
-        if animes["errors"][0]["message"] == "User not found":
+    if "errors" in query:
+        if query["errors"][0]["message"] == "User not found":
             errors.append("User not found")
             return errors
 
-    bulk_add_media = []
-    for list in animes["data"]["MediaListCollection"]["lists"]:
-        if not list["isCustomList"]:
-            for anime in list["entries"]:
-                if anime["status"] == "CURRENT":
-                    status = "Watching"
-                else:
-                    status = anime["status"].capitalize()
+    bulk_add_media = run(anilist_get_media_list(query, errors, user))
 
-                if (
-                    not Media.objects.filter(
-                        media_id=anime["media"]["idMal"],
-                        media_type="anime",
-                        api_origin="mal",
-                        user=user,
-                    ).exists() and anime["media"]["idMal"] is not None):
-
-                    media = Media(
-                            media_id=anime["media"]["idMal"],
-                            title=anime["media"]["title"]["userPreferred"],
-                            media_type="anime",
-                            score=anime["score"],
-                            status=status,
-                            api_origin="mal",
-                            user=user,
-                        )
-                    
-                    img_temp = helpers.get_image_temp(anime["media"]["coverImage"]["medium"])
-                    media.image.save(f'anime-{anime["media"]["coverImage"]["medium"].rsplit("/", 1)[-1]}', File(img_temp), save=False)
-
-                    bulk_add_media.append(
-                        media
-                    )
-
-                else:
-                    errors.append(anime["media"]["title"]["userPreferred"])
-
-    for list in mangas["data"]["MediaListCollection"]["lists"]:
-        if not list["isCustomList"]:
-            for manga in list["entries"]:
-                if manga["status"] == "CURRENT":
-                    status = "Watching"
-                else:
-                    status = manga["status"].capitalize()
-
-                if (
-                    not Media.objects.filter(
-                        media_id=manga["media"]["idMal"],
-                        media_type="manga",
-                        api_origin="mal",
-                        user=user,
-                    ).exists() and manga["media"]["idMal"] is not None):
-
-                    media = Media(
-                            media_id=manga["media"]["idMal"],
-                            title=manga["media"]["title"]["userPreferred"],
-                            media_type="manga",
-                            score=manga["score"],
-                            status=status,
-                            api_origin="mal",
-                            user=user,
-                        )
-                    
-                    img_temp = helpers.get_image_temp(manga["media"]["coverImage"]["medium"])
-                    media.image.save(f'manga-{manga["media"]["coverImage"]["medium"].rsplit("/", 1)[-1]}', File(img_temp), save=False)
-
-                    bulk_add_media.append(
-                        media
-                    )
-                else:
-                    errors.append(manga["media"]["title"]["userPreferred"])
     Media.objects.bulk_create(bulk_add_media)
+
     return errors
+
+
+async def anilist_get_media_list(query, errors, user):
+    async with ClientSession() as session:
+        task = []
+        for media_type in query["data"]:
+            for list in query["data"][media_type]["lists"]:
+                if not list["isCustomList"]:
+                    for content in list["entries"]:
+                        if (
+                            not await Media.objects.filter(
+                                media_id=content["media"]["idMal"],
+                                media_type=media_type,
+                                api_origin="mal",
+                                user=user,
+                            ).aexists() and content["media"]["idMal"] is not None):
+                            task.append(ensure_future(anilist_get_media(session, content, media_type, user)))
+
+                        else:
+                            errors.append(content["media"]["title"]["userPreferred"])
+
+        return await gather(*task)
+
+
+async def anilist_get_media(session, content, media_type, user):
+    if content["status"] == "CURRENT":
+        status = "Watching"
+    else:
+        status = content["status"].capitalize()
+
+    media = Media(
+            media_id=content["media"]["idMal"],
+            title=content["media"]["title"]["userPreferred"],
+            media_type=media_type,
+            score=content["score"],
+            status=status,
+            api_origin="mal",
+            user=user,
+        )
+
+    await helpers.download_image(session, content["media"]["coverImage"]["medium"], media_type)
+        
+    media.image = f"images/{media_type}-{content['media']['coverImage']['medium'].rsplit('/', 1)[-1]}"
+
+    return media
--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,5 +1,10 @@
-import requests
+from django.conf import settings
 from django.core.files.temp import NamedTemporaryFile
+
+import requests
+import aiofiles
+import os
+
 
 def convert_mal_media_type(media_type):
     match media_type:
@@ -33,3 +38,20 @@ def get_image_temp(url):
     img_temp.flush()
 
     return img_temp
+
+
+async def download_image(session, url, media_type):
+    if url == "" or url == "https://image.tmdb.org/t/p/w92None" or url == "https://image.tmdb.org/t/p/w92":
+        url = "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg"
+        location = settings.MEDIA_ROOT + "/images/none.svg"
+    else:
+        location = f"{settings.MEDIA_ROOT}/images/{media_type}-{url.rsplit('/', 1)[-1]}"
+
+    if not os.path.exists(settings.MEDIA_ROOT + "/images"):
+        os.makedirs(settings.MEDIA_ROOT + "/images")
+
+    async with session.get(url) as resp:
+        if resp.status == 200:
+            f = await aiofiles.open(location, mode='wb')
+            await f.write(await resp.read())
+            await f.close()
