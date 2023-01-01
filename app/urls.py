40b6876695c9a49063d81b626a85dd4edb0aa43a
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-01-01 20:32:48 +0100

    add registration parameter

--- app/urls.py
+++ app/urls.py
@@ -3,14 +3,17 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from decouple import config
 from . import views
 
 urlpatterns = [
     path("", views.home, name = "home"),
     path("search/<str:content>/<str:query>/", views.search, name = "search"),
-    path("register/", views.register, name = "register"),
     path("profile/", views.profile, name = "profile"),
     path("login/", views.login, name = "login"),
     path("logout/", auth_views.LogoutView.as_view(), name = "logout"),
     path("edit/<str:media_type>/<int:media_id>/", views.edit, name = "edit"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if config("REGISTRATION", default = False, cast=bool):
+    urlpatterns.append(path("register/", views.register, name = "register"))
