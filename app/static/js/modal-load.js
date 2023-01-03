d35acbcc6f1187067b1aabb2bdb7cc67a38fdbe1
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-01-03 20:10:11 +0100

    adapts to jquery

--- app/static/js/modal-load.js
+++ app/static/js/modal-load.js
@@ -1,74 +1,65 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var buttons = document.getElementsByClassName("open-modal-button");
+$(document).ready(function() {
+    var buttons = $(".open-modal-button");
 
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = function() {
-            var url = this.getAttribute("data-url");
-            var parts = url.split('/');
-            var type = parts[2];
-            var id = parts[3];
-            
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    var data = JSON.parse(xhr.responseText);
-                    if (data.media_type == "light_novel") {
-                        data.media_type = "Light Novel";
-                    } 
-                    else if (data.media_type == "ova") {
-                        data.media_type = "OVA";
-                    } 
-                    else if (data.media_type == "tv") {
-                        data.media_type = "TV";
-                    } 
-                    else {
-                        data.media_type = data.media_type.charAt(0).toUpperCase() + data.media_type.slice(1);
-                    }
-                    var output = data.title;
-                    if (data.year) {
-                        output += " (" + data.year + ")";
-                    }
-                    if (data.media_type) {
-                        output += " [" + data.media_type + "]";
-                    }
+    buttons.click(function() {
+        var url = $(this).data("url");
+        var parts = url.split('/');
+        var type = parts[2];
+        var id = parts[3];
 
-                    document.getElementById("modal-title-" + type + "_" + id).innerHTML = output;
-                    document.getElementById("modal-body-" + type + "_" + id).innerHTML = data.html;
-                    if (data.seasons_details) {
-                        var select = document.getElementById("season-select-" + type + "_" + id);
-
-                        if (select) {
-                            var input = document.getElementById("score-input-" + type + "_" + id);
-                            var status = document.getElementById("season-status-" + type + "_" + id);
-                            select.addEventListener("change", function() {
-                                var selectedValue = select.value;
-                                if (selectedValue in data.seasons_details) {
-                                    input.value = data.seasons_details[selectedValue]["score"];
-                                    status.value = data.seasons_details[selectedValue]["status"];
-                                }
-                                else if (selectedValue == "all") {
-                                    input.value = data.score;
-                                    status.value = data.status;
-                                }
-                                else {                            
-                                    input.value = "";
-                                    status.value = "Completed";
-                                }
-
-                            });
-                        }
-                    }
-
-                    if (!data.in_db) {
-                        document.getElementById("modal-footer-" + type + "_" + id).querySelector('.modal-delete-btn').style.display = "none";
-                      }
-
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(data) {
+                if (data.media_type == "light_novel") {
+                    data.media_type = "Light Novel";
+                } 
+                else if (data.media_type == "ova") {
+                    data.media_type = "OVA";
+                } 
+                else if (data.media_type == "tv") {
+                    data.media_type = "TV";
+                } 
+                else {
+                    data.media_type = data.media_type.charAt(0).toUpperCase() + data.media_type.slice(1);
                 }
-            };
-            
-            xhr.send();
+                var output = data.title;
+                if (data.year) {
+                    output += " (" + data.year + ")";
+                }
+                if (data.media_type) {
+                    output += " [" + data.media_type + "]";
+                }
 
-        };
-    }
+                $("#modal-title-" + type + "_" + id).html(output);
+                $("#modal-body-" + type + "_" + id).html(data.html);
+                if (data.seasons_details) {
+                    var select = $("#season-select-" + type + "_" + id);
+
+                    if (select.length) {
+                        var input = $("#score-input-" + type + "_" + id);
+                        var status = $("#season-status-" + type + "_" + id);
+                        select.change(function() {
+                            var selectedValue = $(this).val();
+                            if (selectedValue in data.seasons_details) {
+                                input.val(data.seasons_details[selectedValue]["score"]);
+                                status.val(data.seasons_details[selectedValue]["status"]);
+                            }
+                            else if (selectedValue == "all") {
+                                input.val(data.score);
+                                status.val(data.status);
+                            }
+                            else {                            
+                                input.val("");
+                                status.val("Completed");
+                            }
+                        });
+                    }
+                }
+                if (!data.in_db) {
+                    $("#modal-footer-" + type + "_" + id + " .modal-delete-btn").hide();
+                }
+            }
+        });
+    });
 });
