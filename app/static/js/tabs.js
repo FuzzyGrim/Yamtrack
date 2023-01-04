0efdb21b06fa205f28a801c50d39823b813ce777
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-01-04 20:26:38 +0100

    fix can't to media type from media type status

--- app/static/js/tabs.js
+++ app/static/js/tabs.js
@@ -3,20 +3,24 @@ var hash = location.hash.replace(/^#/, '');
 if (hash) {
     $('#v-pills-tab button[data-bs-target="#' + hash + '"]').click();
 
-    // add active to media type if hash includes to status
+    // highlight media type if hash includes to status
     var media = hash.split('-')[0];
-    $('#' + media + '-tab').addClass('active');
+    $('#' + media + '-tab').addClass('highlight');
 }
 else {
     $('#v-pills-tab button[data-bs-target="#tv"]').click();
 }
 
-// change hash after click for page-reload
+
 $('#v-pills-tab button').not('.dropdown-toggle').on('click', function () {
+    // change hash after click for page-reload
     var target = $(this).data('bs-target');
     window.location.hash = target;
 
-    // add active to media type if hash includes to status
+    // Remove the "highlight" class from all tab link elements
+    $('#v-pills-tab button').removeClass('highlight');
+
+    // highlight media type if hash includes to status
     var media = target.split('-')[0];
-    $(media + '-tab').addClass('active');
+    $(media + '-tab').addClass('highlight');
 });
