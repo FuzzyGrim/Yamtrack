4cf99081bc4dfb80dc126e12b4eb3d8dfef8219e
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-01-04 12:08:20 +0100

    add active class color

--- app/static/js/tabs.js
+++ app/static/js/tabs.js
@@ -2,14 +2,23 @@
 var hash = location.hash.replace(/^#/, '');
 if (hash) {
     $('#v-pills-tab button[data-bs-target="#' + hash + '"]').click();
+
+    // add active to media type if hash includes to status
+    var media = hash.split('-')[0];
+    $('#' + media + '-tab').addClass('active');
 }
 else {
     $('#v-pills-tab button[data-bs-target="#tv"]').click();
 }
 
-// change hash for page-reload
-$('#v-pills-tab button').not('.dropdown-toggle').on('click', function (e) {
-window.location.hash = $(this).data('bs-target');
+// change hash after click for page-reload
+$('#v-pills-tab button').not('.dropdown-toggle').on('click', function () {
+    var target = $(this).data('bs-target');
+    window.location.hash = target;
+
+    // add active to media type if hash includes to status
+    var media = target.split('-')[0];
+    $(media + '-tab').addClass('active');
 });
 
 // remove hash when search
