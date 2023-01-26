3afde126e3ca68ceb2fdf2be572065bba49b4956
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-01-26 15:03:38 +0100

    run tests separately because of database lock

--- src/app/tests/test_imports.py
+++ src/app/tests/test_imports.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.test import TransactionTestCase
 from django.test import override_settings
 
 import csv
@@ -9,13 +9,13 @@ from app.utils import interactions
 from app.models import User, Media
 
 
-class Imports(LiveServerTestCase):
+class ImportsMAL(TransactionTestCase):
     def setUp(self):
         self.credentials = {"username": "test", "password": "12345"}
         self.user = User.objects.create_user(**self.credentials)
 
 
-    @override_settings(MEDIA_ROOT=("test_Imports/media"))
+    @override_settings(MEDIA_ROOT=("test_ImportsMAL/media"))
     def test_import_animelist(self):
         interactions.import_myanimelist("bloodthirstiness", self.user)
         self.assertEqual(Media.objects.filter(user=self.user).count(), 6)
@@ -25,25 +25,27 @@ class Imports(LiveServerTestCase):
         self.assertEqual(Media.objects.get(user=self.user, title="FLCL").status=="Paused", True)
         self.assertEqual(Media.objects.get(user=self.user, title="Fire Punch").score==7, True)
 
-
-    @override_settings(MEDIA_ROOT=("test_Imports/media"))
-    def test_import_anilist(self):
-        interactions.import_anilist("bloodthirstiness", self.user)
-        self.assertEqual(Media.objects.filter(user=self.user).count(), 6)
-        self.assertEqual(Media.objects.filter(user=self.user, media_type="anime").count(), 4)
-        self.assertEqual(Media.objects.filter(user=self.user, media_type="manga").count(), 2)
-        self.assertEqual(Media.objects.get(user=self.user, title="FLCL").status=="Paused", True)
-        self.assertEqual(Media.objects.get(user=self.user, title="One Punch-Man").score==9, True)
+    def tearDownClass():
+        try:
+            shutil.rmtree("test_ImportsMAL")
+        except OSError:
+            pass
 
 
-    @override_settings(MEDIA_ROOT=("test_Imports/media"))
+class ImportsTMDB(TransactionTestCase):
+    def setUp(self):
+        self.credentials = {"username": "test", "password": "12345"}
+        self.user = User.objects.create_user(**self.credentials)
+
+    @override_settings(MEDIA_ROOT=("test_ImportsTMDB/media"))
     def test_import_tmdb(self):
-        file_path = os.path.join("test_Imports/ratings.csv")
+        file_path = os.path.join("test_ImportsTMDB/ratings.csv")
         fields = ["TMDb ID", "IMDb ID", "Type", "Name", "Release Date", "Season Number", "Episode Number", "Rating", "Your Rating", "Date Rated"]
         data = [
             ["634649", "tt10872600", "movie", "Spider-Man: No Way Home", "2021-12-15T00:00:00Z", "", "", "8.022", "7", "2022-12-17T15:50:35Z"],
             ["1668", "tt0108778", "tv", "Friends", "1994-09-22T00:00:00Z", "", "", "8.463", "10", "2022-12-17T16:23:01Z"],
         ]
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerow(fields)
@@ -57,9 +59,29 @@ class Imports(LiveServerTestCase):
             self.assertEqual(Media.objects.get(user=self.user, media_id=634649).score==7, True)
             self.assertEqual(Media.objects.get(user=self.user, media_id=1668).score==10, True)
 
+    def tearDownClass():
+        try:
+            shutil.rmtree("test_ImportsTMDB")
+        except OSError:
+            pass
+
+
+class ImportsANI(TransactionTestCase):
+    def setUp(self):
+        self.credentials = {"username": "test", "password": "12345"}
+        self.user = User.objects.create_user(**self.credentials)
+
+    @override_settings(MEDIA_ROOT=("test_ImportsANI/media"))
+    def test_import_anilist(self):
+        interactions.import_anilist("bloodthirstiness", self.user)
+        self.assertEqual(Media.objects.filter(user=self.user).count(), 6)
+        self.assertEqual(Media.objects.filter(user=self.user, media_type="anime").count(), 4)
+        self.assertEqual(Media.objects.filter(user=self.user, media_type="manga").count(), 2)
+        self.assertEqual(Media.objects.get(user=self.user, title="FLCL").status=="Paused", True)
+        self.assertEqual(Media.objects.get(user=self.user, title="One Punch-Man").score==9, True)
 
     def tearDownClass():
         try:
-            shutil.rmtree("test_Imports")
+            shutil.rmtree("test_ImportsANI")
         except OSError:
             pass
