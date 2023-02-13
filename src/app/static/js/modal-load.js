e338ee8ab912452bbe606e06d6c281743656d371
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-02-13 12:18:49 +0100

    format js files

--- src/app/static/js/modal-load.js
+++ src/app/static/js/modal-load.js
@@ -1,85 +1,98 @@
-$(document).on("click", ".open-modal-button", function() {
+$(document).on("click", ".open-modal-button", function () {
+  var url = $(this).data("url");
+  var parts = url.split("/");
+  var type = parts[2];
+  var id = parts[3];
 
-    var url = $(this).data("url");
-    var parts = url.split('/');
-    var type = parts[2];
-    var id = parts[3];
+  $.ajax({
+    type: "GET",
+    url: url,
+    success: function (data) {
+      var output = data.title;
+      if (data.year) {
+        output += " (" + data.year + ")";
+      }
 
-    $.ajax({
-        type: 'GET',
-        url: url,
-        success: function(data) {
-            var output = data.title;
-            if (data.year) {
-                output += " (" + data.year + ")";
-            }
+      if (data.original_type) {
+        // capitalize first letter
+        output +=
+          " [" +
+          data.original_type.charAt(0).toUpperCase() +
+          data.original_type.slice(1) +
+          "]";
+      } else if (data.media_type) {
+        // capitalize first letter
+        output +=
+          " [" +
+          data.media_type.charAt(0).toUpperCase() +
+          data.media_type.slice(1);
+        +"]";
+      }
 
-            if (data.original_type) {
-                // capitalize first letter
-                output += " [" + data.original_type.charAt(0).toUpperCase() + data.original_type.slice(1) + "]";
-            }
-            else if (data.media_type) {
-                // capitalize first letter
-                output += " [" + data.media_type.charAt(0).toUpperCase() + data.media_type.slice(1); + "]";
-            }
+      $("#modal-title-" + type + "_" + id).html(output);
+      $("#modal-body-" + type + "_" + id).html(data.html);
+      if (data.media_seasons) {
+        var select = $("#season-select-" + type + "_" + id);
 
-            $("#modal-title-" + type + "_" + id).html(output);
-            $("#modal-body-" + type + "_" + id).html(data.html);
-            if (data.media_seasons) {
-                var select = $("#season-select-" + type + "_" + id);
+        if (select.length) {
+          var score = $("#score-input-" + type + "_" + id);
+          var status = $("#season-status-" + type + "_" + id);
+          var progress = $("#progress-input-" + type + "_" + id);
+          var start = $("#start-input-" + type + "_" + id);
+          var end = $("#end-input-" + type + "_" + id);
 
-                if (select.length) {
-                    var score = $("#score-input-" + type + "_" + id);
-                    var status = $("#season-status-" + type + "_" + id);
-                    var progress = $("#progress-input-" + type + "_" + id);
-                    var start = $("#start-input-" + type + "_" + id);
-                    var end = $("#end-input-" + type + "_" + id);
+          select.change(function () {
+            var selectedValue = $(this).val();
 
-                    select.change(function() {
-                        var selectedValue = $(this).val();
+            if (selectedValue == "general") {
+              score.val(data.score);
+              status.val(data.status);
+              progress.val(data.progress);
+              start.val(data.start_date);
+              end.val(data.end_date);
+            } else {
+              let exists = false;
 
-                        if (selectedValue == "general") {
-                            score.val(data.score);
-                            status.val(data.status);
-                            progress.val(data.progress);
-                            start.val(data.start_date);
-                            end.val(data.end_date);
-                        }
-                        else {
-                            let exists = false;
-
-                            // check if season exists in database
-                            for(let i = 0; i < data["media_seasons"].length && !exists; i++) {
-                                let media_season = data["media_seasons"][i];
-                                if (media_season.number == selectedValue){
-                                    score.val(media_season.score);
-                                    status.val(media_season.status);
-                                    progress.val(media_season.progress);
-                                    start.val(media_season.start_date);
-                                    end.val(media_season.end_date);
-                                    exists = true;
-                                }
-                            }
-
-                            if (!exists) {
-                                score.val("");
-                                status.val("Completed");
-                                progress.val("");
-                                start.val("");
-                                end.val("");
-                            }
-                        }
-                    });
+              // check if season exists in database
+              for (
+                let i = 0;
+                i < data["media_seasons"].length && !exists;
+                i++
+              ) {
+                let media_season = data["media_seasons"][i];
+                if (media_season.number == selectedValue) {
+                  score.val(media_season.score);
+                  status.val(media_season.status);
+                  progress.val(media_season.progress);
+                  start.val(media_season.start_date);
+                  end.val(media_season.end_date);
+                  exists = true;
                 }
-            }
-            if (data.in_db) {
-                // Add delete button if it doesn't exist
-                if ($("#modal-footer-" + type + "_" + id + " .modal-delete-btn").length == 0) {
-                    var deleteButton = $('<button class="btn btn-danger modal-delete-btn" type="submit" name="delete">Delete</button>');
-                    $('#modal-footer-' + type + '_' + id + ' form').append(deleteButton);
-                }
-            }
+              }
 
+              if (!exists) {
+                score.val("");
+                status.val("Completed");
+                progress.val("");
+                start.val("");
+                end.val("");
+              }
+            }
+          });
         }
-    });
+      }
+      if (data.in_db) {
+        // Add delete button if it doesn't exist
+        if (
+          $("#modal-footer-" + type + "_" + id + " .modal-delete-btn").length ==
+          0
+        ) {
+          var deleteButton = $(
+            '<button class="btn btn-danger modal-delete-btn" type="submit" name="delete">Delete</button>'
+          );
+          $("#modal-footer-" + type + "_" + id + " form").append(deleteButton);
+        }
+      }
+    },
+  });
 });
