74a3e12249da6b01f7d42245ee74e4a6de40fcd3
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-02-20 21:39:51 +0100

    split imports forms

--- src/app/static/js/spinner.js
+++ src/app/static/js/spinner.js
@@ -1,16 +1,22 @@
-document.getElementById("import-form").addEventListener("submit", function () {
-  var div = document.createElement("div");
-  div.className = "d-flex justify-content-center";
+// Get all the import forms
+var importForms = document.querySelectorAll(".import");
 
-  var spinner = document.createElement("div");
-  spinner.className = "spinner-border";
-  spinner.setAttribute("role", "status");
+// Add event listeners to each form
+importForms.forEach(function(form) {
+  form.addEventListener("submit", function () {
+    var div = document.createElement("div");
+    div.className = "d-flex justify-content-center";
 
-  var span = document.createElement("span");
-  span.className = "visually-hidden";
-  span.textContent = "Loading...";
+    var spinner = document.createElement("div");
+    spinner.className = "spinner-border";
+    spinner.setAttribute("role", "status");
 
-  spinner.appendChild(span);
-  div.appendChild(spinner);
-  document.querySelector("main").appendChild(div);
+    var span = document.createElement("span");
+    span.className = "visually-hidden";
+    span.textContent = "Loading...";
+
+    spinner.appendChild(span);
+    div.appendChild(spinner);
+    document.querySelector("main").appendChild(div);
+  });
 });
