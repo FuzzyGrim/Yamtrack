9a480339e39e863bc32958593e103c7f9b3ba1cb
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-03-18 17:49:22 +0100

    extend login to use base.html

--- src/app/urls.py
+++ src/app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("medialist/<str:media_type>/<str:status>/", views.medialist, name="medialist"),
     path("search/", views.search, name="search"),
     path("profile/", views.profile, name="profile"),
-    path("login/", views.UpdatedLoginView.as_view(), name="login"),
+    path("login/", views.UpdatedLoginView.as_view(extra_context={'page': 'Login'}), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("edit/", views.edit, name="edit"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
