7362c42f63bce14cbf11b722fddcad99768f9bce
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-03-20 20:33:32 +0100

    remove trailing slashes from url

--- src/app/urls.py
+++ src/app/urls.py
@@ -9,11 +9,11 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("medialist/<str:media_type>/", views.medialist, name="medialist"),
-    path("medialist/<str:media_type>/<str:status>/", views.medialist, name="medialist"),
-    path("search/", views.search, name="search"),
-    path("profile/", views.profile, name="profile"),
-    path("login/", views.UpdatedLoginView.as_view(extra_context={'page': 'Login'}), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("medialist/<str:media_type>/<str:status>", views.medialist, name="medialist"),
+    path("search", views.search, name="search"),
+    path("profile", views.profile, name="profile"),
+    path("login", views.UpdatedLoginView.as_view(extra_context={'page': 'Login'}), name="login"),
+    path("logout", auth_views.LogoutView.as_view(), name="logout"),
     path("edit/", views.edit, name="edit"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
