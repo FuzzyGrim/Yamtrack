5cd112604b49184de8d41a61b7d829312dda45c6
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-04-05 17:56:37 +0200

    simplify and optimize progress component
    instead of passing each attribute for then searching for object, pass object directly instead

--- src/unicorn/components/progress.py
+++ src/unicorn/components/progress.py
@@ -1,31 +1,29 @@
 from django_unicorn.components import UnicornView
-from app.models import Media, Season
+from app.models import Season
 
 
 class ProgressView(UnicornView):
+    progress = 0
+
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
-        self.media_id = kwargs.get("media_id")
-        self.api = kwargs.get("api")
-        self.progress = kwargs.get("progress")
-        self.user_id = kwargs.get("user_id")
-        if "season_number" in kwargs:
-            self.season_number = kwargs.get("season_number")
+        self.media = kwargs.get("media")
+        if hasattr(self.media, "season_number"):
+            self.season_number = self.media.season_number
+            self.progress = self.media.season_progress
+        else:
+            self.progress = self.media.progress
 
     # number is 1 or -1
     def update(self, number):
-
         self.progress += number
-        media = Media.objects.get(
-            user=self.user_id, media_id=self.media_id, api=self.api
-        )
         if hasattr(self, 'season_number'):
             season = Season.objects.get(
-                media_id=media.id,
+                media_id=self.media.id,
                 number=self.season_number,
             )
             season.progress = self.progress
             season.save()
 
-        media.progress += number
-        media.save()
+        self.media.progress += number
+        self.media.save()
