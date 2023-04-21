4b876eb7df1c05c65a92b81fbc1b83244d5e3511
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-04-21 11:34:41 +0200

    add links to details page

--- src/app/utils/details.py
+++ src/app/utils/details.py
@@ -10,10 +10,15 @@ def mal(media_type, media_id):
     cache_key = media_type + str(media_id)
     response = cache.get(cache_key)
     if response is None:
-        url = f"https://api.myanimelist.net/v2/{media_type}/{media_id}?fields=title,main_picture,start_date,synopsis,status,genres,num_episodes,num_chapters,average_episode_duration,related_anime,related_manga,recommendations"
+        url = f"https://api.myanimelist.net/v2/{media_type}/{media_id}?fields=title,main_picture,media_type,start_date,synopsis,status,genres,num_episodes,num_chapters,average_episode_duration,related_anime,related_manga,recommendations"
 
         response = requests.get(url, headers={"X-MAL-CLIENT-ID": MAL_API}).json()
 
+        if response["media_type"] == "tv":
+            response["media_type"] = "anime"
+        response["original_type"] = (
+            response["media_type"].replace("_", " ").title()
+        )
         response["media_type"] = media_type
         response["media_id"] = response["id"]
 
@@ -82,6 +87,7 @@ def tmdb(media_type, media_id):
 
         response = requests.get(url).json()
 
+        response["original_type"] = response["media_type"]
         response["media_type"] = media_type
         response["media_id"] = response["id"]
 
