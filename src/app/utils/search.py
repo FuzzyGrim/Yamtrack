16c94097ee87fdff7277e25e64ccf124e3b65ba1
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-04-29 12:16:15 +0200

    Fix: Better error handling when searching for media

--- src/app/utils/search.py
+++ src/app/utils/search.py
@@ -1,6 +1,8 @@
 from decouple import config
 import requests
+import logging
 
+logger = logging.getLogger(__name__)
 
 TMDB_API = config("TMDB_API", default=None)
 MAL_API = config("MAL_API", default=None)
@@ -8,7 +10,14 @@ MAL_API = config("MAL_API", default=None)
 
 def tmdb(media_type, query):
     url = f"https://api.themoviedb.org/3/search/{media_type}?api_key={TMDB_API}&query={query}"
-    response = requests.get(url).json()["results"]
+    response = requests.get(url).json()
+
+    # when api key is invalid
+    if "success" in response and not response["success"]:
+        logger.error(f"TMDB: {response['status_message']}")
+        return []
+
+    response = response["results"]
     for media in response:
         media["media_id"] = media["id"]
         media["original_type"] = media_type
@@ -25,6 +34,17 @@ def tmdb(media_type, query):
 def mal(media_type, query):
     url = f"https://api.myanimelist.net/v2/{media_type}?q={query}&nsfw=true&fields=media_type"
     response = requests.get(url, headers={"X-MAL-CLIENT-ID": MAL_API}).json()
+
+    if "error" in response:
+        if response["error"] == "forbidden":
+            logger.error(f"MAL: {response['error'].title()}, probably no API key provided")
+        elif response["error"] == "bad_request":
+            if response["message"] == "invalid q":
+                logger.error("MAL: Invalid query")
+            elif response["message"] == "Invalid client id":
+                logger.error("MAL: Wrong API key")
+        return []
+
     if "data" in response:
         response = response["data"]
         for media in response:
