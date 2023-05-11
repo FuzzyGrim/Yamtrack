9d63a005a3af389e7a558f59ac8199750231dc49
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-05-12 00:10:49 +0200

    fix error when submitting episode from a season with missing intermediate episodes

--- src/app/database/episode.py
+++ src/app/database/episode.py
@@ -5,26 +5,37 @@ logger = logging.getLogger(__name__)
 
 
 def add_update_episodes(request, episodes_checked, season_metadata, season_db):
+    """
+    Add new or update existing episodes for a season
+    """
     episodes_to_create = []
     episdoes_to_update = []
+
+    # create dict of episode number and air date pairs
+    # cant get air date with season_metadata["episodes"][episode - 1]["air_date"]
+    # because there could be missing episodes in the middle
+    if "release" in request.POST:
+        air_dates = {}
+        for episode_metadata in season_metadata["episodes"]:
+            if episode_metadata['episode_number'] in episodes_checked:
+                air_dates[episode_metadata['episode_number']] = episode_metadata['air_date']
 
     for episode in episodes_checked:
         episode = int(episode)
         if "release" in request.POST:
-            # set watch date to air date if available
-            if season_metadata["episodes"][episode - 1]["air_date"]:
-                watch_date = season_metadata["episodes"][episode - 1]["air_date"]
-            else:
-                watch_date = None
+            # set watch date to air date
+            # air date could be null
+            watch_date = air_dates.get(episode)
         else:
             # set watch date from form
             watch_date = request.POST.get("date")
 
+        # update episode watch date
         try:
             episode_db = Episode.objects.get(season=season_db, number=episode)
             episode_db.watch_date = watch_date
             episdoes_to_update.append(episode_db)
-
+        # create new episode if it doesn't exist
         except Episode.DoesNotExist:
             episode_db = Episode(
                 season=season_db,
