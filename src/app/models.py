d08e76c26e0e90c5cc6bf604a67b98b5cd6c0ee1
FuzzyGrim <stromed_chariotman@aleeas.com>
2023-06-19 18:14:15 +0200

    restructure models

--- src/app/models.py
+++ src/app/models.py
@@ -1,65 +1,85 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.db.models import Min, Max
+from django.core.validators import MinValueValidator, MaxValueValidator, DecimalValidator
 
 
 class Media(models.Model):
-    media_id = models.IntegerField()
-    title = models.CharField(max_length=256)
+    media_id = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
     image = models.ImageField()
-    media_type = models.CharField(
-        max_length=10,
-        choices=[
-            ("anime", "Anime"),
-            ("manga", "Manga"),
-            ("tv", "TV"),
-            ("movie", "Movie"),
-        ],
-    )
-    score = models.DecimalField(null=True, max_digits=3, decimal_places=1)
-    progress = models.IntegerField()
-    status = models.CharField(max_length=30)
-    # Allow null values for start date for myanimelist and anilist imports
-    start_date = models.DateField(null=True)
-    end_date = models.DateField(null=True)
-    notes = models.TextField(blank=True)
+    score = models.DecimalField(null=True, blank=True, max_digits=3, decimal_places=1, validators=[DecimalValidator(3, 1), MinValueValidator(0), MaxValueValidator(10)])
+    progress = models.PositiveIntegerField(null=True, blank=True)
+    status = models.CharField(max_length=10, default="Completed", choices=[
+        ("Completed", "Completed"),
+        ("Watching", "Watching"),
+        ("Paused", "Paused"),
+        ("Dropped", "Dropped"),
+        ("Planning", "Planning"),
+    ])
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.title} ({self.media_id})"
 
     class Meta:
+        abstract = True
         ordering = ["-score"]
 
 
-class Season(models.Model):
-    parent = models.ForeignKey(Media, on_delete=models.CASCADE, related_name="seasons")
-    image = models.ImageField()
-    number = models.IntegerField()
-    score = models.DecimalField(null=True, max_digits=3, decimal_places=1)
-    status = models.CharField(max_length=30)
-    progress = models.IntegerField()
-    # Allow null values for start date for myanimelist and anilist imports
-    start_date = models.DateField(null=True)
-    end_date = models.DateField(null=True)
-    notes = models.TextField(blank=True)
+class TV(Media):
+    @property
+    def progress(self):
+        return sum(season.progress for season in self.seasons.all())
+
+    status = None
+    start_date = None
+    end_date = None
+
+
+class Season(Media):
+    tv_show = models.ForeignKey(TV, on_delete=models.CASCADE, related_name="seasons")
+    season_number = models.PositiveIntegerField()
+
+    @property
+    def progress(self):
+        return sum(episode.watched for episode in self.episodes.all())
+
+    @property
+    def start_date(self):
+        return self.episode_set.aggregate(start_date=Min('watched_date'))['start_date']
+
+    @property
+    def end_date(self):
+        return self.episode_set.aggregate(end_date=Max('watched_date'))['end_date']
 
     def __str__(self):
-        return f"{self.parent} - S{self.number}"
-
-    class Meta:
-        ordering = ["parent", "-number"]
+        return f"{self.tv_show} - S{self.season_number}"
 
 
 class Episode(models.Model):
-    season = models.ForeignKey(
-        Season, on_delete=models.CASCADE, related_name="episodes"
-    )
-    number = models.IntegerField()
-    watch_date = models.DateField(null=True)
+    tv_season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name="episodes")
+    watched = models.BooleanField(default=False)
+    watched_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.season}E{self.number}"
+        return f"{self.tv_season} - {self.title}"
+
+
+class Manga(Media):
+    pass
+
+
+class Anime(Media):
+    pass
+
+
+class Movie(Media):
+    progress = None
 
 
 class User(AbstractUser):
