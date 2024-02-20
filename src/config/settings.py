6eeea5c3f74b86254024814a5b240faa40ef4f96
FuzzyGrim <stromed_chariotman@aleeas.com>
2024-02-20 10:47:04 +0100

    remove some password validators

--- src/config/settings.py
+++ src/config/settings.py
@@ -124,16 +124,7 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
