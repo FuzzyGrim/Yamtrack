9379019c2cd178fc7405677ba4e1f6f58cb42048
FuzzyGrim <stromed_chariotman@aleeas.com>
2024-03-05 11:36:51 +0100

    fix default layout migration

--- src/users/migrations/0006_alter_user_anime_layout_alter_user_manga_layout_and_more.py
+++ src/users/migrations/0006_alter_user_anime_layout_alter_user_manga_layout_and_more.py
@@ -3,36 +3,69 @@
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration):
+def change_layouts(apps, schema_editor):
+    """Layouts names have been changed, so we need to update them."""
 
+    User = apps.get_model("users", "User")
+    for user in User.objects.all():
+        user.anime_layout = "table"
+        user.manga_layout = "table"
+        user.tv_layout = "grid"
+        user.season_layout = "grid"
+        user.movie_layout = "grid"
+        user.save()
+
+
+class Migration(migrations.Migration):
     dependencies = [
-        ('users', '0005_remove_user_default_layout_user_anime_layout_and_more'),
+        ("users", "0005_remove_user_default_layout_user_anime_layout_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='anime_layout',
-            field=models.CharField(choices=[('grid', 'Grid'), ('table', 'Table')], default='table', max_length=20),
+            model_name="user",
+            name="anime_layout",
+            field=models.CharField(
+                choices=[("grid", "Grid"), ("table", "Table")],
+                default="table",
+                max_length=20,
+            ),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='manga_layout',
-            field=models.CharField(choices=[('grid', 'Grid'), ('table', 'Table')], default='table', max_length=20),
+            model_name="user",
+            name="manga_layout",
+            field=models.CharField(
+                choices=[("grid", "Grid"), ("table", "Table")],
+                default="table",
+                max_length=20,
+            ),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='movie_layout',
-            field=models.CharField(choices=[('grid', 'Grid'), ('table', 'Table')], default='grid', max_length=20),
+            model_name="user",
+            name="movie_layout",
+            field=models.CharField(
+                choices=[("grid", "Grid"), ("table", "Table")],
+                default="grid",
+                max_length=20,
+            ),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='season_layout',
-            field=models.CharField(choices=[('grid', 'Grid'), ('table', 'Table')], default='grid', max_length=20),
+            model_name="user",
+            name="season_layout",
+            field=models.CharField(
+                choices=[("grid", "Grid"), ("table", "Table")],
+                default="grid",
+                max_length=20,
+            ),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='tv_layout',
-            field=models.CharField(choices=[('grid', 'Grid'), ('table', 'Table')], default='grid', max_length=20),
+            model_name="user",
+            name="tv_layout",
+            field=models.CharField(
+                choices=[("grid", "Grid"), ("table", "Table")],
+                default="grid",
+                max_length=20,
+            ),
         ),
+        migrations.RunPython(change_layouts),
     ]
