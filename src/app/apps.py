e51833d279882dc3ec41e84d14b21b363fa298a6
FuzzyGrim <stromed_chariotman@aleeas.com>
2024-04-04 16:35:11 +0200

    remove getting igdb access token on startup

--- src/app/apps.py
+++ src/app/apps.py
@@ -1,14 +1,8 @@
 from django.apps import AppConfig
 
-from app.providers import igdb
-
 
 class AppConfig(AppConfig):
     """Default app config."""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
-
-    def ready(self):
-        """Run when the app is ready."""
-        igdb.get_access_token()
