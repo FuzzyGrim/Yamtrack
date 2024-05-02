85e05913032fe769273c1013b4e053d973ac1584
FuzzyGrim <stromed_chariotman@aleeas.com>
2024-05-02 21:20:40 +0200

    fix crash caused by titles with only special characters

--- src/app/templatetags/app_extras.py
+++ src/app/templatetags/app_extras.py
@@ -20,8 +20,18 @@ def no_under(arg1):
 
 @register.filter()
 def slug(arg1):
-    """Return the slug of the string."""
-    return template.defaultfilters.slugify(unidecode(arg1))
+    """Return the slug of the string.
+
+    Sometimes slugify removes all characters from a string, so we need to
+    urlencode the special characters first.
+    e.g Anime: 31687
+    """
+    cleaned = template.defaultfilters.slugify(unidecode(arg1))
+    if cleaned == "":
+        return template.defaultfilters.slugify(
+            template.defaultfilters.urlencode(unidecode(arg1)),
+        )
+    return cleaned
 
 
 @register.filter()
