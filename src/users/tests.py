e8e8b3fc4be45f73b1a0dd883f7a6f335ff44046
FuzzyGrim <stromed_chariotman@aleeas.com>
2024-05-03 20:51:22 +0200

    add tests for user tasks view

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,6 +1,9 @@
+import json
+
 from django.contrib import auth
 from django.test import TestCase
 from django.urls import reverse
+from django_celery_results.models import TaskResult
 
 from users.models import User
 
@@ -37,3 +40,41 @@ class Profile(TestCase):
             },
         )
         self.assertEqual(auth.get_user(self.client).check_password("*FNoZN64"), True)
+
+
+class TaskViewTests(TestCase):
+    """Test user task views."""
+
+    def setUp(self):
+        """Create user for the tests."""
+        self.credentials = {"username": "test", "password": "12345"}
+        self.user = User.objects.create_user(**self.credentials)
+        self.client.login(**self.credentials)
+
+    def test_task_status_handling(self):
+        """Test if view correctly modifies the task results based on their status."""
+        TaskResult.objects.create(
+            task_args=json.dumps(["<SimpleLazyObject: <User: test>>"]),
+            result=json.dumps({"exc_message": ["Error occurred"]}),
+            task_id="1",
+            status="FAILURE",
+        )
+        TaskResult.objects.create(
+            task_args=json.dumps(["<SimpleLazyObject: <User: test>>"]),
+            result=json.dumps({}),
+            task_id="2",
+            status="STARTED",
+        )
+        TaskResult.objects.create(
+            task_args=json.dumps(["<SimpleLazyObject: <User: test>>"]),
+            result=None,
+            task_id="3",
+            status="PENDING",
+        )
+
+        response = self.client.get(reverse("tasks"))
+        tasks = response.context["tasks"]
+
+        self.assertEqual(tasks[2].result, "Error occurred")
+        self.assertEqual(tasks[1].result, "Task in progress")
+        self.assertEqual(tasks[0].result, "Waiting for task to start")
