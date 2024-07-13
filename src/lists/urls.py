7fd1f84ceeb409ebf329f3f27f662b9e268feb50
FuzzyGrim <stromed_chariotman@aleeas.com>
2024-07-13 23:03:01 +0200

    update list create, edit and delete urls

--- src/lists/urls.py
+++ src/lists/urls.py
@@ -4,10 +4,10 @@ from lists import views
 
 urlpatterns = [
     path("lists", views.lists, name="lists"),
-    path("list/<int:list_id>", views.list_detail, name="list_detail"),
-    path("lists/create", views.create, name="create"),
-    path("lists/edit", views.edit, name="edit"),
-    path("lists/delete", views.delete, name="delete"),
     path("lists_modal", views.lists_modal, name="lists_modal"),
+    path("list/<int:list_id>", views.list_detail, name="list_detail"),
+    path("list/create", views.create, name="create"),
+    path("list/edit", views.edit, name="edit"),
+    path("list/delete", views.delete, name="delete"),
     path("list_item_toggle", views.list_item_toggle, name="list_item_toggle"),
 ]
