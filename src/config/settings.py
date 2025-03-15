b290963f116acc7d92399765c96b3558f1cc11ad
FuzzyGrim <stromed_chariotman@aleeas.com>
2025-03-15 20:05:02 +0100

    only use celery_ping to check celery

--- src/config/settings.py
+++ src/config/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
     "health_check.cache",
     "health_check.storage",
     "health_check.contrib.migrations",
-    "health_check.contrib.celery",
     "health_check.contrib.celery_ping",
     "health_check.contrib.redis",
     "allauth",
