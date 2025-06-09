170d73da9eba073c157a0c783124562306fd8364
Ricardo Gonzalez <rickgc@gmail.com>
2025-06-09 17:52:26 +0100

    Add tests

---
+++ src/integrations/tests/test_webhooks_emby.py
@@ -0,0 +1,226 @@
+import json
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from app.models import TV, Episode, Item, MediaTypes, Movie, Season, Status
+
+
+class EmbyWebhookTests(TestCase):
+    """Tests for Emby webhook."""
+
+    def setUp(self):
+        """Set up test data."""
+        self.client = Client()
+        self.credentials = {"username": "testuser", "token": "test-token"}
+        self.user = get_user_model().objects.create_superuser(**self.credentials)
+        self.url = reverse("emby_webhook", kwargs={"token": "test-token"})
+
+    def test_invalid_token(self):
+        """Test webhook with invalid token returns 401."""
+        url = reverse("emby_webhook", kwargs={"token": "invalid-token"})
+        response = self.client.post(url, data={}, content_type="application/json")
+        self.assertEqual(response.status_code, 401)
+
+    def test_tv_episode_mark_played(self):
+        """Test webhook handles TV episode mark played event."""
+        payload = {
+            "Event": "playback.stop",
+            "Item": {
+                "Type": "Episode",
+                "Name": "The One Where Monica Gets a Roommate",
+                "ProductionYear": 1994,
+                "ProviderIds": {
+                    "Tvdb": "303821",
+                    "Imdb": "tt0583459",
+                },
+                "SeriesName": "Friends",
+                "IndexNumber": 1,
+                "ParentIndexNumber": 1,
+            },
+            "PlaybackInfo": {
+                "PlayedToCompletion": True,
+            },
+        }
+
+        data = {
+            "data": json.dumps(payload),
+        }
+
+        response = self.client.post(
+            self.url,
+            data=data,
+            format="multipart",
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        # Verify objects were created
+        tv_item = Item.objects.get(media_type=MediaTypes.TV.value, media_id="1668")
+        self.assertEqual(tv_item.title, "Friends")
+
+        tv = TV.objects.get(item=tv_item, user=self.user)
+        self.assertEqual(tv.status, Status.IN_PROGRESS.value)
+
+        season = Season.objects.get(
+            item__media_id="1668",
+            item__season_number=1,
+        )
+        self.assertEqual(season.status, Status.IN_PROGRESS.value)
+
+        episode = Episode.objects.get(
+            item__media_id="1668",
+            item__season_number=1,
+            item__episode_number=1,
+        )
+        self.assertIsNotNone(episode.end_date)
+
+    def test_movie_mark_played(self):
+        """Test webhook handles movie mark played event."""
+        payload = {
+            "Event": "playback.stop",
+            "Item": {
+                "Type": "Movie",
+                "Name": "The Matrix",
+                "ProductionYear": 1999,
+                "ProviderIds": {
+                    "Imdb": "tt0133093",
+                    "Tmdb": "603",
+                    "Tvdb": "169",
+                    "Official Website": "http://www.warnerbros.com/matrix",
+                    "Wikidata": "Q83495",
+                    "Wikipedia": "The_Matrix",
+                },
+            },
+            "PlaybackInfo": {
+                "PlayedToCompletion": True,
+            },
+        }
+        data = {
+            "data": json.dumps(payload),
+        }
+
+        response = self.client.post(
+            self.url,
+            data=data,
+            format="multipart",
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        # Verify movie was created and marked as completed
+        movie = Movie.objects.get(
+            item__media_id="603",
+            user=self.user,
+        )
+        self.assertEqual(movie.status, Status.COMPLETED.value)
+        self.assertEqual(movie.progress, 1)
+
+    def test_ignored_event_types(self):
+        """Test webhook ignores irrelevant event types."""
+        payload = {
+            "Event": "playback.something_else",
+            "Item": {
+                "Type": "Movie",
+                "Name": "The Matrix",
+                "ProductionYear": 1999,
+                "ProviderIds": {
+                    "Imdb": "tt0133093",
+                    "Tmdb": "603",
+                    "Tvdb": "169",
+                    "Official Website": "http://www.warnerbros.com/matrix",
+                    "Wikidata": "Q83495",
+                    "Wikipedia": "The_Matrix",
+                },
+            },
+            "PlaybackInfo": {
+                "PlayedToCompletion": True,
+            },
+        }
+
+        data = {
+            "data": json.dumps(payload),
+        }
+
+        response = self.client.post(
+            self.url,
+            data=data,
+            format="multipart",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Movie.objects.count(), 0)
+
+    def test_missing_tmdb_id(self):
+        """Test webhook handles missing TMDB ID gracefully."""
+        payload = {
+            "Event": "playback.stop",
+            "Item": {
+                "Type": "Movie",
+                "Name": "The Matrix",
+                "ProductionYear": 1999,
+                "ProviderIds": {},
+            },
+            "PlaybackInfo": {
+                "PlayedToCompletion": True,
+            },
+        }
+        data = {
+            "data": json.dumps(payload),
+        }
+
+        response = self.client.post(
+            self.url,
+            data=data,
+            format="multipart",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Movie.objects.count(), 0)
+
+    def test_repeated_watch(self):
+        """Test webhook handles repeated watches."""
+        payload = {
+            "Event": "playback.stop",
+            "Item": {
+                "Type": "Movie",
+                "Name": "The Matrix",
+                "ProductionYear": 1999,
+                "ProviderIds": {
+                    "Imdb": "tt0133093",
+                    "Tmdb": "603",
+                    "Tvdb": "169",
+                    "Official Website": "http://www.warnerbros.com/matrix",
+                    "Wikidata": "Q83495",
+                    "Wikipedia": "The_Matrix",
+                },
+            },
+            "PlaybackInfo": {
+                "PlayedToCompletion": True,
+            },
+        }
+
+        data = {
+            "data": json.dumps(payload),
+        }
+
+        # First watch
+        response = self.client.post(
+            self.url,
+            data=data,
+            format="multipart",
+        )
+
+        # Second watch
+        response = self.client.post(
+            self.url,
+            data=data,
+            format="multipart",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        movie = Movie.objects.filter(item__media_id="603")
+        self.assertEqual(movie.count(), 2)
+        self.assertEqual(movie[0].status, Status.COMPLETED.value)
+        self.assertEqual(movie[1].status, Status.COMPLETED.value)
