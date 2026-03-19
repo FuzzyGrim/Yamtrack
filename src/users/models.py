2ed617d79e1eab90ff3bd2b1a6b41800861738c1
Stefan Boxleitner <19754081+boxleits@users.noreply.github.com>
2026-03-19 12:30:19 +0100

    Add LATEST_EPISODE sort choice to MediaSortChoices

--- src/users/models.py
+++ src/users/models.py
@@ -38,6 +38,7 @@ class MediaSortChoices(models.TextChoices):
     PROGRESS = "progress", "Progress"
     START_DATE = "start_date", "Start Date"
     END_DATE = "end_date", "End Date"
+    LATEST_EPISODE = "latest_episode", "Latest Aired Episode"
 
 
 class MediaStatusChoices(models.TextChoices):
