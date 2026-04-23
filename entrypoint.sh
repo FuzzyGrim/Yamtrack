d47f55b53e1693cb70655bb0de78a87af9b35e7d
Mr. Rubber Ducky <contact@rubberverse.xyz>
2026-04-23 17:13:40 +0200

    feat: rootless container
    Hopefully this is descriptive enough to explain what was changed. All container logs from processes are accessible within container in `/tmp` now so no easy direct logs in stdout.
    
    Dockerfile:
    - Create an user abc:abc with UID and GID of 1000
    - Change ownership of directories to abc:abc and set more restrictive file & directory permissions
    - Remove shadow package after installation
    - Drop sed -i command for nginx IPv6, instead it now runs as dual-stack (it works fine in my setup, though I can revert this change if desired)
    - Run python manage.py collectstatic --noinput as abc:abc user instead of root
    - Container runs entrypoint as abc:abc user (1000:1000)
    
    supervisord.conf:
    - Remove user elevation so supervisord doesn't panic on launch
    - Remove /dev/stderr, /dev/stdout from log directives as it needs root privileges to attach to them
    - Save temporary supervisord logs to /tmp
    - Remove IPv6 environment variable from nginx command
    
    nginx.conf:
    - Make nginx write it's logs and files to /tmp to avoid permission denied issues (supervisord still redirects output from console to /tmp)
    - Listen on both `:8000` and `[::]:8000`
    
    src/config/settings.py
    - Added `USE_X_FORWARDED_HOST = True` as otherwise django was not trusting Caddy's headers after reverse proxying, breaking SSO auth flow by setting callback URL to local IP address.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,4 @@ set -e
 
 python manage.py migrate --noinput
 
-PUID=${PUID:-1000}
-PGID=${PGID:-1000}
-
-groupmod -o -g "$PGID" abc
-usermod -o -u "$PUID" abc
-
-chown abc:abc /yamtrack
-chown -R abc:abc db
-chown -R abc:abc staticfiles
-chown -R abc:abc /var/log/nginx
-chown -R abc:abc /var/lib/nginx
-
 exec /usr/local/bin/supervisord -c /etc/supervisord.conf
