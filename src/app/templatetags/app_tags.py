5dd99739a0760893456245e4222f1e57ece9a952
FuzzyGrim <stromed_chariotman@aleeas.com>
2026-05-01 15:22:17 +0200

    replace episode watch time js with django values

--- src/app/templatetags/app_tags.py
+++ src/app/templatetags/app_tags.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 from django import template
@@ -111,6 +112,16 @@ def datetime_format(datetime, user):
         formatted_time = formats.time_format(local_dt, user.time_format)
         return f"{formatted_date} {formatted_time}"
     return formatted_date
+
+
+@register.simple_tag
+def now_plus_minutes(minutes):
+    """Return a date/datetime-local value for now plus minutes."""
+    minutes = int(minutes)
+    local_dt = timezone.localtime(timezone.now() + timedelta(minutes=minutes))
+    if settings.TRACK_TIME:
+        return local_dt.strftime("%Y-%m-%dT%H:%M")
+    return local_dt.strftime("%Y-%m-%d")
 
 
 @register.filter
