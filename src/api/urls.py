ae97cc1f83c2cad95e06688c3a286bed3b88e123
bunz <66bunz@gmail.com>
2026-07-06 11:55:32 +0200

    feat(api): Fix wrong url_name to make Swagger UI work

--- src/api/urls.py
+++ src/api/urls.py
@@ -176,7 +176,7 @@ if settings.SPECTACULAR_ENABLE_SERVE:
         re_path(r"^schema/?$", SpectacularAPIView.as_view(), name="api_schema"),
         re_path(
             r"^docs/?$",
-            SpectacularSwaggerView.as_view(url_name="api_docs"),
+            SpectacularSwaggerView.as_view(url_name="api_schema"),
             name="api_docs",
         ),
     ]
