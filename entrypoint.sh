1df4268529d1b41c78977f6228ac683f5c4b717d
bunz <66bunz@gmail.com>
2026-08-23 14:44:36 +0200

    feat(configurable-port): Make YAMTRACK_INTERNAL_PORT rewrite idempotent

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,21 @@ groupmod -o -g "$PGID" abc
 usermod -o -u "$PUID" abc
 
 YAMTRACK_INTERNAL_PORT=${YAMTRACK_INTERNAL_PORT:-8000}
-sed -i "s/8000/$YAMTRACK_INTERNAL_PORT/g" /etc/nginx/nginx.conf /etc/nginx/nginx.ipv6.conf
+case "$YAMTRACK_INTERNAL_PORT" in
+    ''|*[!0-9]*)
+        echo "Invalid YAMTRACK_INTERNAL_PORT: '$YAMTRACK_INTERNAL_PORT' (must be a number)" >&2
+        exit 1
+        ;;
+esac
+if [ "$YAMTRACK_INTERNAL_PORT" -lt 1 ] || [ "$YAMTRACK_INTERNAL_PORT" -gt 65535 ]; then
+    echo "Invalid YAMTRACK_INTERNAL_PORT: '$YAMTRACK_INTERNAL_PORT' (must be between 1 and 65535)" >&2
+    exit 1
+fi
+
+sed -i \
+    -e "s/listen [0-9]\{1,5\};/listen ${YAMTRACK_INTERNAL_PORT};/" \
+    -e "s/listen \[::\]:[0-9]\{1,5\};/listen [::]:${YAMTRACK_INTERNAL_PORT};/" \
+    /etc/nginx/nginx.conf /etc/nginx/nginx.ipv6.conf
 
 chown abc:abc /yamtrack
 chown -R abc:abc db
